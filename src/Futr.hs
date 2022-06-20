553cabcc1c3222574357ba37fb192ebfe308c8c0
prolic <saschaprolic@googlemail.com>
2022-06-20 18:11:13 -0400

    factor out model updaters to futr

--- src/Futr.hs
+++ src/Futr.hs
@@ -7,13 +7,16 @@ import Control.Lens
 import Crypto.Schnorr (XOnlyPubKey)
 import Data.DateTime
 import Data.Default
+import Data.List (find, sortBy)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Text (Text)
 
 import Nostr.Event
 import Nostr.Keys
 import Nostr.Profile
+import Nostr.Relay
 
 data FutrModel = FutrModel
   { _time             :: DateTime
@@ -29,3 +32,29 @@ instance Default FutrModel where
   def = FutrModel (fromSeconds 0) initialKeys Map.empty [] Map.empty
 
 makeLenses 'FutrModel
+
+updateContacts :: Map XOnlyPubKey (Profile, DateTime) -> [(XOnlyPubKey, (Profile, DateTime))] -> Map XOnlyPubKey (Profile, DateTime)
+updateContacts original new =
+  Map.union newContacts original
+  where
+    newContacts = Map.fromList $ catMaybes $ map (checkContactIsNewer original) new
+
+checkContactIsNewer :: Map XOnlyPubKey (Profile, DateTime) -> (XOnlyPubKey, (Profile, DateTime)) -> Maybe (XOnlyPubKey, (Profile, DateTime))
+checkContactIsNewer original (xo, (p, d)) =
+  case Map.lookup xo original of
+    Nothing ->
+      Just (xo, (p, d))
+    Just (p', d') ->
+      if d' > d
+        then Just (xo, (p', d'))
+        else Nothing
+
+addEvent :: [ReceivedEvent] -> Event -> Relay -> [ReceivedEvent]
+addEvent re e r = sortBy sortByDate $ addedEvent : newList
+  where
+    addedEvent = case find (dupEvent e) re of
+      Just (e', rs) -> (e', r : filter (\r' -> not $ r `sameRelay` r') rs)
+      _             -> (e, [r])
+    newList = filter (not . dupEvent e) re
+    dupEvent e' re' = e' == fst re'
+    sortByDate a b = compare (created_at $ fst b) (created_at $ fst a)
--- src/Main.hs
+++ src/Main.hs
@@ -294,22 +294,6 @@ timerLoop sendMsg = void $ forever $ do
 
 -- subscriptions
 
-updateContacts :: Map XOnlyPubKey (Profile, DateTime) -> [(XOnlyPubKey, (Profile, DateTime))] -> Map XOnlyPubKey (Profile, DateTime)
-updateContacts original new =
-  Map.union newContacts original
-  where
-    newContacts = Map.fromList $ catMaybes $ map (checkContactIsNewer original) new
-
-checkContactIsNewer :: Map XOnlyPubKey (Profile, DateTime) -> (XOnlyPubKey, (Profile, DateTime)) -> Maybe (XOnlyPubKey, (Profile, DateTime))
-checkContactIsNewer original (xo, (p, d)) =
-  case Map.lookup xo original of
-    Nothing ->
-      Just (xo, (p, d))
-    Just (p', d') ->
-      if d' > d
-        then Just (xo, (p', d'))
-        else Nothing
-
 closeSubscriptions :: MVar RelayPool -> TChan Request -> Maybe SubscriptionId -> IO ()
 closeSubscriptions pool request subId = do
   case subId of
@@ -379,16 +363,6 @@ loadContacts pool request model sendMsg = do
 tagToProfile :: DateTime -> Tag -> Maybe (XOnlyPubKey, (Profile, DateTime))
 tagToProfile datetime (PTag (ValidXOnlyPubKey xo) _ name) = Just (xo,  ( Profile (fromMaybe "" name) Nothing Nothing Nothing, datetime))
 tagToProfile _ _ = Nothing
-
-addEvent :: [ReceivedEvent] -> Event -> Relay -> [ReceivedEvent]
-addEvent re e r = sortBy sortByDate $ addedEvent : newList
-  where
-    addedEvent = case find (dupEvent e) re of
-      Just (e', rs) -> (e', r : filter (\r' -> not $ r `sameRelay` r') rs)
-      _             -> (e, [r])
-    newList = filter (not . dupEvent e) re
-    dupEvent e' re' = e' == fst re'
-    sortByDate a b = compare (created_at $ fst b) (created_at $ fst a)
 
 sendPost :: TChan Request -> FutrModel -> Text -> IO ()
 sendPost request model post = do
