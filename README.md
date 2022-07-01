73b9a778c0f1bf213319cfcfe34ed824b6022388
prolic <saschaprolic@googlemail.com>
2022-07-01 02:56:07 -0400

    add nix support

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 nostr client desktop app written in haskel.
 
 The name 'futr' was chosen from respect for the honorable Canadians.
+
+
+## Setup with Nix
+
+- install nix
+- setup cache
+- $ nix build
+- $ nix-build
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "futr nostr desktop client";
+
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+  };
+
+  outputs = inputs:
+    let
+      makeNixpkgs = system:
+        let projectOverlay = final: prev: {
+          project = final.haskell-nix.project' {
+            src = ./.;
+            compiler-nix-name = "ghc8107";
+
+            shell.tools = {
+              cabal = { };
+              ghcid = { };
+            };
+          };
+        };
+
+        in
+        import inputs.nixpkgs {
+          inherit system;
+          inherit (inputs.haskell-nix) config;
+          overlays = [ inputs.haskell-nix.overlay projectOverlay ];
+        };
+
+      flake = inputs.flake-utils.lib.eachDefaultSystem (system:
+        (makeNixpkgs system).project.flake { });
+    in
+    inputs.nixpkgs.lib.attrsets.recursiveUpdate flake {
+      # Must cross-compile from x86_64-linux only
+      packages.x86_64-linux.default =
+        (makeNixpkgs "x86_64-linux").pkgsCross.musl64.project.hsPkgs.futr.components.exes.futr;
+    };
+}
