01a5695bccfe26aae317ea71346a7000d452aaef
prolic <saschaprolic@googlemail.com>
2022-07-25 01:40:07 -0400

    final image

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-./linuxdeploy-x86_64.AppImage --appdir futr.AppDir
-
-mkdir -p futr.AppDir/usr/bin
-mkdir -p futr.AppDir/usr/share/fonts/truetype/Roboto
-mkdir -p futr.AppDir/usr/share/icons/futr
-
-cp build/futr futr.AppDir/usr/bin
+cp -R assets futr.AppDir/usr/bin
 cp assets/icons/futr-icon.png futr.AppDir
-cp assets/fonts/* futr.AppDir/usr/share/fonts/truetype/Roboto
-cp assets/icons/* futr.AppDir/usr/share/icons/futr
-
+linuxdeploy-x86_64.AppImage --appdir futr.AppDir
 ARCH=x86_64 ./appimagetool-x86_64.AppImage futr.AppDir
