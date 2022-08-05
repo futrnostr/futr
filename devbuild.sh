47066fca6da1e805c589ca371dc89a9cdfdd33ef
prolic <saschaprolic@googlemail.com>
2022-08-05 17:50:00 -0400

    fix dev build docker image

--- devbuild.sh
+++ devbuild.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+RUN="docker run -it -v ${PWD}:/app -w /app prolic/futr-dev:latest"
 stack build --copy-bins --local-bin-path futr.AppDir/usr/bin
-docker run -it -v ${PWD}:/app -w /app prolic/futr-dev:latest ./build-appimage.sh
+$RUN cp -R assets futr.AppDir/usr/bin
+$RUN cp assets/icons/futr-icon.png futr.AppDir
+$RUN linuxdeploy-x86_64.AppImage --appdir futr.AppDir
+ARCH=x86_64 $RUN appimagetool-x86_64.AppImage futr.AppDir
