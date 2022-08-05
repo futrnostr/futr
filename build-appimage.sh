bce67d306eb35ab1637f18bae9704170939ed452
prolic <saschaprolic@googlemail.com>
2022-08-05 18:01:40 -0400

    update build app image script

--- build-appimage.sh
+++ build-appimage.sh
@@ -3,4 +3,4 @@
 cp -R assets futr.AppDir/usr/bin
 cp assets/icons/futr-icon.png futr.AppDir
 APPIMAGE_EXTRACT_AND_RUN=1 linuxdeploy-x86_64.AppImage --appdir futr.AppDir
-ARCH=x86_64 appimagetool-x86_64.AppImage futr.AppDir --appimage-extract-and-run
+ARCH=x86_64 appimagetool-x86_64.AppImage futr.AppDir
