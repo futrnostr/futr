896d6d1d183f771bc612f31edf8e0323a8be3d1b
prolic <saschaprolic@googlemail.com>
2024-09-19 19:01:00 -0400

    Make AppImage work again
    resolves https://github.com/futrnostr/futr/issues/19

--- devbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-RUN="docker run -it -v ${PWD}:/app -w /app prolic/futr-dev:latest"
-stack build --copy-bins --local-bin-path futr.AppDir/usr/bin
-$RUN cp -R assets futr.AppDir/usr/bin
-$RUN cp assets/icons/futr-icon.png futr.AppDir
-$RUN linuxdeploy-x86_64.AppImage --appdir futr.AppDir
-ARCH=x86_64 $RUN appimagetool-x86_64.AppImage futr.AppDir
