aa2c9537a5febe4adb163491eae88b0fe6bdbcaf
prolic <saschaprolic@googlemail.com>
2025-02-21 16:41:55 -0300

    add helper functions for publish status updates

--- src/Nostr/Publisher.hs
+++ src/Nostr/Publisher.hs
@@ -1,12 +1,12 @@
 module Nostr.Publisher where
 
-import Control.Monad (forM, forM_, void, when)
+import Control.Monad (forM, forM_, void)
 import Data.List (nub, partition)
 import Data.Map.Strict qualified as Map
 import Data.Set qualified as Set
 import Data.Text (Text)
 import Effectful
-import Effectful.Concurrent (Concurrent)
+import Effectful.Concurrent (Concurrent, threadDelay)
 import Effectful.Concurrent.Async (async)
 import Effectful.Concurrent.STM (atomically, writeTChan)
 import Effectful.Dispatch.Dynamic (interpret)
@@ -87,12 +87,7 @@ runPublisher =  interpret $ \_ -> \case
                 map getUri outboxCapable ++
                 concatMap (map getUri) followerRelays
 
-        modify $ \st' -> st' 
-            { publishStatus = Map.insert 
-                (eventId event')
-                (Map.fromList [(relay, Publishing) | relay <- allTargetRelays]) 
-                (publishStatus st') 
-            }
+        initEventPublishStatus (eventId event') allTargetRelays
         
         existingConnections <- getConnectedRelays
         let (existingRelays, newRelays) = partition (`elem` existingConnections) allTargetRelays
@@ -105,13 +100,8 @@ runPublisher =  interpret $ \_ -> \case
                 then do
                     writeToChannel event' r
                     disconnect r
-                else do
-                    modify $ \st' -> st'
-                        { publishStatus = Map.adjust
-                            (Map.insert r (Failure "Relay server unreachable"))
-                            (eventId event')
-                            (publishStatus st')
-                        }
+                else
+                    updateEventRelayStatus (eventId event') r (Failure "Relay server unreachable")
 
     PublishToOutbox event' -> do
         void $ putEvent $ EventWithRelays event' Set.empty
@@ -122,23 +112,13 @@ runPublisher =  interpret $ \_ -> \case
         generalRelayList <- getGeneralRelays pk
         let outboxCapableURIs = map getUri $ filter isOutboxCapable generalRelayList
 
-        modify $ \st -> st 
-            { publishStatus = Map.insert 
-                (eventId event') 
-                (Map.fromList [(relay, Publishing) | relay <- outboxCapableURIs]) 
-                (publishStatus st) 
-            }
+        initEventPublishStatus (eventId event') outboxCapableURIs
 
         forM_ outboxCapableURIs $ \r -> writeToChannel event' r
 
     PublishToRelay event' relayUri' -> do
         void $ putEvent $ EventWithRelays event' $ Set.empty
-        modify $ \st -> st 
-            { publishStatus = Map.adjust
-                (\existingMap -> Map.insert relayUri' Publishing existingMap)
-                (eventId event')
-                (publishStatus st)
-            }
+        updateEventRelayStatus (eventId event') relayUri' Publishing
         writeToChannel event' relayUri'
 
     PublishGiftWrap event' senderPk recipientPk -> do
@@ -151,12 +131,7 @@ runPublisher =  interpret $ \_ -> \case
             else do
                 let allRelayURIs = nub $ dmRelayList ++ recipientDMRelays
 
-                modify $ \st -> st
-                    { publishStatus = Map.insert
-                        (eventId event')
-                        (Map.fromList [(relay, Publishing) | relay <- allRelayURIs])
-                        (publishStatus st)
-                    }
+                initEventPublishStatus (eventId event') allRelayURIs
 
                 existingConnections <- getConnectedRelays
                 let (existingRelays, newRelays) = partition 
@@ -169,14 +144,11 @@ runPublisher =  interpret $ \_ -> \case
                     if connected
                         then do
                             writeToChannel event' r
+                            -- Delay disconnect to allow message transmission
+                            threadDelay 1000000  -- 1 second delay
                             disconnect r
-                        else do
-                            modify $ \st' -> st'
-                                { publishStatus = Map.adjust
-                                    (Map.insert r (Failure "Relay server unreachable"))
-                                    (eventId event')
-                                    (publishStatus st')
-                                }
+                        else
+                            updateEventRelayStatus (eventId event') r (Failure "Relay server unreachable")
 
     GetPublishResult eventId' -> do
         st <- get @RelayPool
@@ -195,9 +167,9 @@ writeToChannel e r = do
     case Map.lookup r (activeConnections st) of
         Just rd -> do
             atomically $ writeTChan (requestChannel rd) (SendEvent e)
-            modify $ \st' -> st' { publishStatus = Map.adjust (Map.insert r WaitingForConfirmation) (eventId e) (publishStatus st') }
-        Nothing -> do
-            modify $ \st' -> st' { publishStatus = Map.adjust (Map.insert r (Failure "No channel found")) (eventId e) (publishStatus st') }
+            updateEventRelayStatus (eventId e) r WaitingForConfirmation
+        Nothing ->
+            updateEventRelayStatus (eventId e) r (Failure "No active connection")
 
 
 -- | Get the connected relays
@@ -205,3 +177,25 @@ getConnectedRelays :: PublisherEff es => Eff es [RelayURI]
 getConnectedRelays = do
     st <- get @RelayPool
     return $ Map.keys $ Map.filter ((== Connected) . connectionState) (activeConnections st)
+
+
+-- | Update publish status for an event at a specific relay
+updateEventRelayStatus :: PublisherEff es => EventId -> RelayURI -> PublishStatus -> Eff es ()
+updateEventRelayStatus eid relay status =
+    modify $ \st -> st
+        { publishStatus = Map.adjust
+            (Map.insert relay status)
+            eid
+            (publishStatus st)
+        }
+
+
+-- | Initialize publish status for an event with multiple relays
+initEventPublishStatus :: PublisherEff es => EventId -> [RelayURI] -> Eff es ()
+initEventPublishStatus eid rs =
+    modify $ \st -> st
+        { publishStatus = Map.insert
+            eid
+            (Map.fromList [(relay, Publishing) | relay <- rs])
+            (publishStatus st)
+        }
