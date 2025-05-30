4e184605f7d6d3c7554e0d39c4e9b8ecdd5ff0f0
prolic <saschaprolic@googlemail.com>
2025-05-29 22:28:26 -0300

    set dev version 0.3.0.1

--- src/Version.hs
+++ src/Version.hs
@@ -4,7 +4,7 @@ module Version where
 
 -- | Version string extracted from cabal file
 versionString :: String
-versionString = "0.3.0.0"
+versionString = "0.3.0.1"
 
 -- | Runtime version in 'v0.4.0' format
 runtimeVersion :: String
